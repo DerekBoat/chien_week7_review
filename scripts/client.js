c80862b9756ecf61e2a0da676d5d1a5ce0eb4671
Dev Jana <devjana@Devs-MacBook-Pro-2.local>
2020-01-31 15:20:56 -0600

    user input collected

--- scripts/client.js
+++ scripts/client.js
@@ -2,4 +2,16 @@ $( document ).ready( onReady );
 
 function onReady(){
     console.log( 'in onReady' );
+    $( '#addItemButton' ).on( 'click', addItem );
 } // end onReady    
+
+function addItem(){
+    console.log( 'in addItem' );
+    // get user input & create a new item object
+    let newItem = {
+        description: $( '#descriptionIn' ).val(),
+        size: $( '#sizeIn' ).val(),
+        color: $( '#colorIn' ).val()
+    } // end newItem
+    console.log( 'adding:', newItem );
+} // end addItem
